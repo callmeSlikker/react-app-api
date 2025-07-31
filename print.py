c3e229829495bbf34dd15d258472c7e73fca4fb2
callmeSlikker <131174425+callmeSlikker@users.noreply.github.com>
2025-07-31 11:15:32 +0700

    delete backend

--- print.py
+++
@@ -1,4 +0,0 @@
-responseArray = result.stdout.strip().split('\n')
-print("responseArray"+ responseArray)
-parsedApiResults = json.loads(responseArray[6].split(".py ")[1])
-print("parsedApiResults"+ parsedApiResults)
